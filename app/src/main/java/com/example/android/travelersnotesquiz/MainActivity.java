c19302024b34468f3206a27f4d0836e780db6d1b
Svetlana <svetlanalearn@mail.ru>
2018-02-06 18:40:06 +0800

    added text about correct/incorrect answer after user tap on button 'View result'

--- app/src/main/java/com/example/android/travelersnotesquiz/MainActivity.java
+++ app/src/main/java/com/example/android/travelersnotesquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.travelersnotesquiz;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,44 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int TOTAL_QUESTIONS = 6;
     public static final String FINAL_SCORE_STATUS_KEY = "finalScoreStatusKey";
-    public static final String RIGHT_ANSWERS_STATUS_KEY = "rightAnswersStatus_Key";
+    public static final String RIGHT_ANSWERS_STATUS_KEY = "rightAnswersStatusKey";
+    public static final String ISRIGHT_ANSWERS_STATUS_KEY = "isrightAnswersStatusKey";
     boolean finalScoreStatus = false;
     boolean rightAnswersStatus = false;
+    boolean isrightAnswersStatus = false;
 
+    /**
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState != null) {
-            if (savedInstanceState.containsKey(FINAL_SCORE_STATUS_KEY)) {
-                finalScoreStatus = savedInstanceState.getBoolean(FINAL_SCORE_STATUS_KEY);
-                if (finalScoreStatus) {
-                    showTotalScore(null);
-                }
-            }
-            if (savedInstanceState.containsKey(RIGHT_ANSWERS_STATUS_KEY)) {
-                rightAnswersStatus = savedInstanceState.getBoolean(RIGHT_ANSWERS_STATUS_KEY);
-                if (rightAnswersStatus) {
-                    viewAnswers(null);
-                }
-            }
-        }
-    }
-
-    /**
-     * This method is called when the View result button is clicked.
-     */
-    public void showTotalScore(View view) {
-        ImageView bangkokairwaysImage = findViewById(R.id.stuffBangkokAirways);
-        bangkokairwaysImage.setImageResource(R.drawable.bangkokairwaysanswer);
-        String message = finalText(measureScore());
-        displayMessage(message);
-        showToast(getString(R.string.you_final_result), measureScore());
-
-        TextView finalScoreTextView = findViewById(R.id.finalScore);
-        finalScoreTextView.setVisibility(View.VISIBLE);
-
-        finalScoreStatus = true;
     }
 
     /**
@@ -64,10 +40,145 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         outState.putBoolean(FINAL_SCORE_STATUS_KEY, finalScoreStatus);
         outState.putBoolean(RIGHT_ANSWERS_STATUS_KEY, rightAnswersStatus);
+        outState.putBoolean(ISRIGHT_ANSWERS_STATUS_KEY, isrightAnswersStatus);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (savedInstanceState.containsKey(FINAL_SCORE_STATUS_KEY)) {
+            finalScoreStatus = savedInstanceState.getBoolean(FINAL_SCORE_STATUS_KEY);
+            if (finalScoreStatus) {
+                showTotalScore(null);
+            }
+        }
+        if (savedInstanceState.containsKey(RIGHT_ANSWERS_STATUS_KEY)) {
+            rightAnswersStatus = savedInstanceState.getBoolean(RIGHT_ANSWERS_STATUS_KEY);
+            if (rightAnswersStatus) {
+                viewAnswers(null);
+            }
+        }
+        if (savedInstanceState.containsKey(ISRIGHT_ANSWERS_STATUS_KEY)) {
+            isrightAnswersStatus = savedInstanceState.getBoolean(ISRIGHT_ANSWERS_STATUS_KEY);
+            if (isrightAnswersStatus) {
+                showAnswerCorrectnessTextView(null);
+            }
+        }
     }
 
     /**
-     * This method is called when the answers button is clicked.
+     * This method is called when the View result button is clicked.
+     */
+    public void showTotalScore(View view) {
+        //change image for question about Bangkok Airways
+        ImageView bangkokairwaysImage = findViewById(R.id.stuffBangkokAirways);
+        bangkokairwaysImage.setImageResource(R.drawable.bangkokairwaysanswer);
+
+        // display final user's score
+        String message = finalText(measureScore());
+        displayMessage(message);
+
+        // toast with final user's score
+        showToast(getString(R.string.you_final_result), measureScore());
+
+        // display text view wuth score
+        TextView finalScoreTextView = findViewById(R.id.finalScore);
+        finalScoreTextView.setVisibility(View.VISIBLE);
+
+        // to save status if user rotates the phone
+        finalScoreStatus = true;
+        isrightAnswersStatus = true;
+
+        showAnswerCorrectness();
+        showAnswerCorrectnessTextView(null);
+    }
+
+    /**
+     * This method set Text for the correct/incorrect answer
+     */
+    public String answersCorrectnessText(boolean correctness) {
+        if (correctness) {
+            return getString(R.string.right_answer);
+        } else {
+            return getString(R.string.wrong_answer);
+        }
+    }
+
+    /**
+     * This method set Color for the correct/incorrect answer
+     */
+    public int answersCorrectnessColor(boolean correctness) {
+        if (correctness) {
+            return Color.rgb(0, 255, 0);
+        } else {
+            return Color.rgb(255, 0, 0);
+        }
+    }
+
+    /**
+     * This method shows Text for the correct/incorrect answer
+     */
+    public void showAnswerCorrectness() {
+        // BangkokAirways
+        TextView correctTextViewBangkokAirways = findViewById(R.id.isRightAnswerBangkokAirways);
+        correctTextViewBangkokAirways.setText(answersCorrectnessText(questionBangkokAirways()));
+        correctTextViewBangkokAirways.setTextColor(answersCorrectnessColor(questionBangkokAirways()));
+
+        // ThaiNewYear
+        TextView correctTextViewThaiNewYear = findViewById(R.id.isRightAnswerThaiNewYear);
+        correctTextViewThaiNewYear.setText(answersCorrectnessText(questionThaiNewYear()));
+        correctTextViewThaiNewYear.setTextColor(answersCorrectnessColor(questionThaiNewYear()));
+
+        // Spacewar
+        TextView correctTextViewSpacewar = findViewById(R.id.isRightAnswerSpacewar);
+        correctTextViewSpacewar.setText(answersCorrectnessText(questionSpacewar()));
+        correctTextViewSpacewar.setTextColor(answersCorrectnessColor(questionSpacewar()));
+
+        // Durian
+        TextView correctTextViewDurian = findViewById(R.id.isRightAnswerDurian);
+        correctTextViewDurian.setText(answersCorrectnessText(questionDurian()));
+        correctTextViewDurian.setTextColor(answersCorrectnessColor(questionDurian()));
+
+        // KualaLumpur
+        TextView correctTextViewKualaLumpur = findViewById(R.id.isRightAnswerKualaLumpur);
+        correctTextViewKualaLumpur.setText(answersCorrectnessText(questionKualaLumpur()));
+        correctTextViewKualaLumpur.setTextColor(answersCorrectnessColor(questionKualaLumpur()));
+
+        // Bangkok
+        TextView correctTextViewBangkok = findViewById(R.id.isRightAnswerBangkok);
+        correctTextViewBangkok.setText(answersCorrectnessText(questionBangkok()));
+        correctTextViewBangkok.setTextColor(answersCorrectnessColor(questionBangkok()));
+    }
+
+    /**
+     * This method shows TextViews with Correctness answer
+     */
+    public void showAnswerCorrectnessTextView(View view) {
+        TextView correctnessBangkokTextView = findViewById(R.id.isRightAnswerBangkok);
+        correctnessBangkokTextView.setVisibility(View.VISIBLE);
+
+        TextView correctnessBangkokAirwaysTextView = findViewById(R.id.isRightAnswerBangkokAirways);
+        correctnessBangkokAirwaysTextView.setVisibility(View.VISIBLE);
+
+        TextView correctnessDurianTextView = findViewById(R.id.isRightAnswerDurian);
+        correctnessDurianTextView.setVisibility(View.VISIBLE);
+
+        TextView correctnessKualaLumpurTextView = findViewById(R.id.isRightAnswerKualaLumpur);
+        correctnessKualaLumpurTextView.setVisibility(View.VISIBLE);
+
+        TextView correctnessSpacewarTextView = findViewById(R.id.isRightAnswerSpacewar);
+        correctnessSpacewarTextView.setVisibility(View.VISIBLE);
+
+        TextView correctnessThaiNewYearTextView = findViewById(R.id.isRightAnswerThaiNewYear);
+        correctnessThaiNewYearTextView.setVisibility(View.VISIBLE);
+
+        isrightAnswersStatus = true;
+    }
+
+    /**
+     * This method shows hidden answers. is called when the answers button is clicked.
      */
     public void viewAnswers(View view) {
         TextView bangkokTextView = findViewById(R.id.rightAnswerBangkok);
@@ -272,7 +383,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method displays create final text with score result
+     * This method displays created final text with score result
      */
     private String finalText(int score) {
         return getString(R.string.your_total_score, score, TOTAL_QUESTIONS);
